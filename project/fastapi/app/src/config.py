082951fe1b726fab7abe01f1dc9f49e499262e94
nimannv <nimannv@gmail.com>
2024-11-11 00:15:00 +0330

    connet fastapi to gRPC server

--- project/fastapi/app/src/config.py
+++ project/fastapi/app/src/config.py
@@ -1,8 +1,7 @@
-# src.config
 from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    GRPC_ADDRESS: str = ""
+    GRPC_ADDRESS: str = "grpc_server:50051"
 
 settings = Config()
--- project/fastapi/app/src/consumption/router.py
+++ project/fastapi/app/src/consumption/router.py
@@ -4,18 +4,25 @@ from ..config import settings
 
 from datetime import datetime
 
+import grpc
+from ..consumption.proto import metric_service_pb2, metric_service_pb2_grpc
 
 router = APIRouter()
 
 @router.get("/get_consumptions")
 async def get_consumptions(start_time: datetime, end_time: datetime) -> schemas.get_consumptions_res:
 
+    with grpc.insecure_channel(settings.GRPC_ADDRESS) as channel:
+        stub = metric_service_pb2_grpc.MetricServiceStub(channel)
+        request = metric_service_pb2.MetricRequest(start_time=start_time, end_time=end_time)
+        response = stub.GetMetrics(request)
+
         res = []
-        for dp in range(5):
+        for dp in response.data_points:
             res.append(
                 schemas.data_point(
-                    time=datetime.utcfromtimestamp(1731246688),
-                    value=2.5
+                    time=datetime.utcfromtimestamp(dp.time.seconds),
+                    value=dp.value
                 )
             )
 
