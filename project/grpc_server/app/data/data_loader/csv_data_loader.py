bcdabf8812ec23162052088459c3d6d77f17ada2
nimannv <nimannv@gmail.com>
2024-11-11 16:02:45 +0330

    aware datetime UTC

--- project/grpc_server/app/data/data_loader/csv_data_loader.py
+++ project/grpc_server/app/data/data_loader/csv_data_loader.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import List
 import math
 
@@ -8,7 +8,7 @@ from data.metric.data_point import DataPoint
 
 
 class CSVDataLoader(DataLoader):
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
         self.file_path = file_path
 
     def get_data_between(self, start_time: datetime, end_time: datetime) -> List[DataPoint]:
@@ -16,9 +16,10 @@ class CSVDataLoader(DataLoader):
         with open(self.file_path, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                time = datetime.strptime(row['time'], '%Y-%m-%d %H:%M:%S')
-                if start_time <= time <= end_time:
+                time = datetime.strptime(row['time'], '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+                if start_time <= time < end_time:
                     value = float(row['meterusage'])
+                    # NaN value is possible in CSV file
                     if math.isnan(value):
                         value = None
                     data_points.append(DataPoint(time, value))
