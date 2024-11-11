a46a272413fc128f8eb47b9857a1b0294ecb3727
nimannv <nimannv@gmail.com>
2024-11-11 16:10:11 +0330

    test influx connection

--- project/grpc_server/app/test/test.py
+++ project/grpc_server/app/test/test.py
@@ -1,21 +1,31 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from data.data_loader.data_loader_factory import DataLoaderFactory
 from data.metric.metric import Metric
 
 def test_csv():
     
-    csv_loader = DataLoaderFactory.create_data_loader(
-        'csv',
-        file_path='meterusage.csv'
-    )
-    metric_csv = Metric(csv_loader)
+    csv_loader = DataLoaderFactory.create_csv_loader('meterusage.csv')
+    metric = Metric(csv_loader)
     
-    start_time = datetime.strptime("2019-01-01 01:00:00", '%Y-%m-%d %H:%M:%S')
-    end_time = datetime.strptime("2019-01-01 02:00:00", '%Y-%m-%d %H:%M:%S')
+    start_time = datetime.strptime("2019-01-01 01:00:00", '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+    end_time = datetime.strptime("2019-01-01 02:00:00", '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
 
-    csv_data = metric_csv.get_data_between(start_time, end_time)
+    csv_data = metric.get_data_between(start_time, end_time)
     for data in csv_data:
         print(data.time, data.value)
+
+def influx_test():
+    data_loader = DataLoaderFactory.create_influxdb_loader('http://localhost:8086', 'mytoken', 'spectral')
+    metric = Metric(data_loader)
+    
+    start_time = datetime.strptime("2019-01-01 01:00:00", '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+    end_time = datetime.strptime("2019-01-01 02:00:00", '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone.utc)
+
+    csv_data = metric.get_data_between(start_time, end_time)
+    for data in csv_data:
+        print(data.time, data.value)
+        
+
 
 
 
