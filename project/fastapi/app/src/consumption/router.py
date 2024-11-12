50dbbdc39c84cc875510fc7b6713a8e59c424f38
nimannv <nimannv@gmail.com>
2024-11-12 15:27:54 +0330

    cleaning

--- project/fastapi/app/src/consumption/router.py
+++ project/fastapi/app/src/consumption/router.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException, APIRouter
-from src.consumption import dependencies, schemas, service
+from src.consumption import schemas, service
 
 from datetime import datetime
 
@@ -8,10 +8,10 @@ router = APIRouter()
 @router.get("/get_consumptions")
 async def get_consumptions(start_time: datetime, end_time: datetime) -> schemas.get_consumptions_res:
 
-        rRPC_response = await service.get_data_from_gRPC(start_time, end_time)
+        gRPC_response = await service.get_data_from_gRPC(start_time, end_time)
 
         res = []
-        for dp in rRPC_response.data_points:
+        for dp in gRPC_response.data_points:
             res.append(
                 schemas.data_point(
                     time=datetime.utcfromtimestamp(dp.time.seconds),
