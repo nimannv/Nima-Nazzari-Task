d39de8f47559aad742c8180a856993def3cbcdac
nimannv <nimannv@gmail.com>
2024-11-12 15:24:43 +0330

    cleaning

--- project/grpc_server/app/data/data_loader/influxdb_data_loader.py
+++ project/grpc_server/app/data/data_loader/influxdb_data_loader.py
@@ -1,6 +1,7 @@
 from influxdb_client import InfluxDBClient
 from datetime import datetime
 from typing import List
+import os
 
 from data.data_loader.base import DataLoader
 from data.metric.data_point import DataPoint
@@ -12,6 +13,9 @@ class InfluxDBDataLoader(DataLoader):
             token=token,
             org=org
         )
+        self._bucket = 'spectral'
+        self._measurement = 'meterusage'
+        self._field = 'meterusage'
     
     def get_data_between(self, start_time: datetime, end_time: datetime) -> List[DataPoint]:
         
@@ -22,10 +26,10 @@ class InfluxDBDataLoader(DataLoader):
         query_api = self.client.query_api()
 
         query = f'''
-            from(bucket: "spectral")
+            from(bucket: "{self._bucket}")
             |> range(start: {start_time_str}, stop: {end_time_str})
-            |> filter(fn: (r) => r["_measurement"] == "meterusage")
-            |> filter(fn: (r) => r["_field"] == "meterusage")
+            |> filter(fn: (r) => r["_measurement"] == "{self._measurement}")
+            |> filter(fn: (r) => r["_field"] == "{self._field}")
         '''
 
         results = query_api.query(query)
